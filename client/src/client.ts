6d53507d640ce1adaea1506148f58a04ce4f00fe
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-09 01:35:23 +0530

    feat: client links
    feat: client links

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,11 +1,12 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
 import type { AppRouter } from "../../server/src";
 
 export const client = createTRPCProxyClient<AppRouter>({
   links: [
+    loggerLink(),
     httpBatchLink({
       url: "http://localhost:3000/trpc",
+      headers: { Authorization: "TOKEN" },
     }),
   ],
 });
-
